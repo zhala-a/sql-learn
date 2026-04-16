e2f486daa872d7ca6ed23e7f98fb219835fa8f9c
o.aliyev <o.alkiyev@asan.gov.az>
2026-04-16 19:36:27 +0400

    test commit

--- lessons/01-fundamentals/exercises.sql
+++ lessons/01-fundamentals/exercises.sql
@@ -1,5 +1,5 @@
 -- ===========================================
--- Lesson 1: SQL Fundamentals - Practice Exercises
+-- Lesson 1 test: SQL Fundamentals - Practice Exercises
 -- GameVerse Database
 -- ===========================================
 
